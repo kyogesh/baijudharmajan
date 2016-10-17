f3e7b74d7fb51536e802d194676618c7f6a33d75
Yogesh Kumar <yk.301189@gmail.com>
2016-10-17 06:10:52 +0530

    Updated allowed hosts

--- baijudharmajan/prod.py
+++ baijudharmajan/prod.py
@@ -2,4 +2,4 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['baiju-dharmajan.com', '139.59.15.173']
+ALLOWED_HOSTS = ['baiju-dharmajan.com', 'www.baiju-dharmajan.com','139.59.15.173']

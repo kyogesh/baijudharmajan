c6b9e5e304269f15c960114536c69a64c370fcfd
Yogesh Kumar <yk.301189@gmail.com>
2017-05-09 15:38:41 +0530

    Changed model field

--- web/models.py
+++ web/models.py
@@ -15,7 +15,7 @@ class ContactFormDetail(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    phone = models.IntegerField()
+    phone = models.CharField(max_length=15, null=True, blank=True)
     regarding = models.CharField(max_length=15, choices=[('None', 'none'),
         ('none', None), ('events', 'Events'), ('promo', 'Promotion'),
         ('endorse', 'Endorsement'), ('collab', 'Collaboration')])
